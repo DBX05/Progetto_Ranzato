f5843bad50e2dd6f5c754c947223c570526679db
DBX05 <riccardoconte76@gmail.com>
2026-03-04 13:06:19 +0100

    Aggiunta file per gli errori e prime modifiche alle classi di data e mese

--- fileCpp/dataora/dataora.h
+++ fileCpp/dataora/dataora.h
@@ -32,31 +32,106 @@ class data
 {
     private:
         // consiglio: se volessimo stampare il nome abbreviato alle prime 3 lettere, mettere una funzione di troncamento che stampa fino alla terza lettera
-        enum enumGiornoSettimana{ lunedi, martedi, mercoledi, giovedi, venerdi, sabato, domenica };
+        std::string Settimana[7] = { "lunedi", "martedi", "mercoledi", "giovedi", "venerdi", "sabato", "domenica" };
+        unsigned int st;
+        std::string stcor;
     public:
-        data();
-        // riceve un interno compreso da 0 e 6 compreso, restituisce una stringa con il giorno della settimana
-        std::string getGiornoSettimana(int) const;
-        // metodi void: cambiare il tipo di ritorno con quello adeguato se necessario
-        void modificaData();
-        void systemDay() const;
+        
+
+        /*
+        prende un intero con valore compreso da 0 e 6 compreso e default 0;
+        se il valore è valido inizializza l'oggetto;
+        altrimenti lancia un'eccezzione (da gestire).
+        */
+        data(unsigned int x = 0): st((x > -1 || x < 8) ? x : -1){
+            if(st == -1) throw data();
+            stcor = Settimana[st];
+        };
+        
+        // ritorna la data dell'oggetto corrente
+        std::string getGiorno(int) const{
+            return Settimana[st];
+        };
+        
+        /*
+        cambia il valore dell'oggetto data
+        */        void modificaData(unsigned int x){
+            if (x < 0 || x > 7) throw data();
+            st = x;
+            stcor = Settimana[st];
+        };
+        //Possibile aggiunta futura: void systemDay() const;
+
+        /* 
+        Prende in input un'oggetto di tipo data
+        Confronta i valori di due date
+        ritorna 0 se sono uguali
+        -1 se l'oggetto corretnte è maggiore
+        1 se l'oggetto corrente è minore
+        */
+
+        // potrebbe essere portata esterna come overloading dell'operatore di confronto (da valutare)
+        unsigned int ConfGrioni(unsigned int x) const{
+            if(st == x) return 0;
+            if(st > x) return -1;
+            return 1;
+        }
 };
 
 class mese
 {
     private:
         // aggiungere funzione che passato il mese come parametro (o indice dell'enumMese) restituisce i giorni di quel mese?
-        enum enumMese{ gennaio, febbraio, marzo, aprile, maggio, giugno, luglio, agosto, settembre, ottobre, novembre, dicembre };
+        std::string Mesi[12] = {"gennaio", "febbraio", "marzo", "aprile", "maggio", "giugno", "luglio", "agosto", "settembre", "ottobre", "novembre", "dicembre"};
+        unsigned int ms;
+        std::string mscor; // variabile che tiene la stringa del mese, con scopo di velocizzare la stampa dello stesso (valuare l'utilità)
+
     public:
-        mese();
-        // riceve un interno compreso da 0 e 11 compreso, restituisce una stringa con il mese
-        // possibile variante con secondo parametro intero: bit 0 o 1 per restituire rispettivamente stringa o indice stesso? Utile per il rispondere alla domanda sopra
-        std::string getMese(int) const;
-        // metodi void: cambiare il tipo di ritorno con quello adeguato se necessario
-        void modificaMese();
-        void systemMonth() const;
+        /*
+        prende un intero con valore compreso da 0 e 11 compreso e default 0;
+        se il valore è valido inizializza l'oggetto;
+        altrimenti lancia un'eccezzione (da gestire).
+        */
+
+        //Versione 1
+        mese(unsigned int x = 0): ms((x < 12 || x > -1) ? x : -1) {
+            if(ms == -1) throw mese();
+            mscor = Mesi[ms];
+        };
+
+        // ritorna il mese dell'oggetto corrente
+        std::string getMese() const{
+            return Mesi[ms];
+        };
+
+
+        /*
+        cambia il valore dell'oggetto mese
+        */
+        void modificaMese(unsigned int x) {
+            if (x < 0 || x > 11) throw mese();
+            ms = x;
+            mscor = Mesi[ms];
+        }
+        // Possibile aggiunta futura : void systemMonth() const;
+
+        /* 
+        Prende in input un'oggetto di tipo mese
+        Confronta i valori di due mesi
+        ritorna 0 se sono uguali
+        -1 se l'oggetto corretnte è maggiore
+        1 se l'oggetto corrente è minore
+        */
+
+        // potrebbe essere portata esterna come overloading dell'operatore di confronto (da valutare)
+        unsigned int ConfMesi(unsigned int x) const{
+            if(ms == x) return 0;
+            if(ms > x) return -1;
+            return 1;
+        }
 
 };
+
 
 class anno
 {
